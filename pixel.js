016a30b6a3b53be9274c9f2b0169ee468922d980
sagerioz <ericaepperson27@yahoo.com>
2017-02-11 14:03:54 -0700

    paintbrush effect in EFFECT

--- pixel.js
+++ pixel.js
@@ -23,16 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
         cont.appendChild(createDivWithClass('pixel'));
     }
 
-//---------------------------color functionality--------------------------------
-
-    function colorMe() {
-        if (event.target.className === 'pixel') {
-            event.target.classList.toggle('red');
-            console.log("target hit", event.target);
-        }
-    }
-    cont.addEventListener("click", colorMe)
-
 //----------------------------------make palette--------------------------------
 
     var palette = createDivWithClass('palette')
@@ -41,43 +31,83 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function makePalette(num) {
         for (var i = 0; i < num; i++) {
-            var paintBlobs = createDivWithClass('blobs diamond')
-            palette.appendChild(paintBlobs)
+            var paintChips = createDivWithClass('chips diamond')
+            palette.appendChild(paintChips)
         }
     }
-    makePalette(10);
+    makePalette(25);
+//-------------------------make current paint chip------------------------------
+
+
+
+//---------------------------color functionality--------------------------------
+var color='';
+function chooseColor(){
+  if (event.target.className === 'chips diamond') {
+
+    color = event.target.getAttribute("style");
+      console.log(event.target);
+      console.log("new color choice", color);
+  }
+}
+function colorMe() {
+    if (event.target.className === 'pixel') {
+        event.target.style = color;
+        console.log("COLOR", color);
+        console.log("target hit", event.target);
+        console.log("BACKGROUND STYLE", event.target.style);
+        console.log("BACKGROUND COLOR", event.target.style.backgroundColor);
+        cont.addEventListener('mouseover', colorMe)
+        cont.addEventListener('mouseup', removeTheEventListener)
+
+    }
+}
 
     //----------------------------------button---------------------------------
 
     var butn = document.createElement('button');
-
-    butn.setAttribute('content', 'test content');
     butn.setAttribute('class', 'btn');
-    butn.innerHTML = 'Click for Pallete';
+    butn.innerHTML = 'Choose New Palette';
 
     var wrapper = document.getElementById("divWrapper");
     wrapper.appendChild(butn);
-
     butn.addEventListener("click", button_click)
-
-    function button_click() {
-        var paintBlobs = document.getElementsByClassName('blobs')
-        console.log("PAINTBLOBS", paintBlobs);
 
 //-----------------------------random color picker------------------------------
 
-        for (var i = 0; i < paintBlobs.length; i++) {
-            console.log(paintBlobs[i]);
-            paintBlobs[i].style.backgroundColor = '#' + Math.floor((Math.random() * 0xF00000) + 0x0FFFFF).toString(16);
+    function button_click() {
+        var paintChips = document.getElementsByClassName('chips')
+        console.log("PAINTCHIPS", paintChips);
+        for (var i = 0; i < paintChips.length; i++) {
+            console.log(paintChips[i]);
+            paintChips[i].style.backgroundColor = '#' + Math.floor((Math.random() * 0xF00000) + 0x0FFFFF).toString(16);
         }
     }
+//---------------------------reset the canvas-----------------------------------
+
+var butn_Reset = document.createElement('button');
+butn_Reset.setAttribute('class', 'btn')
+butn_Reset.innerText = "Start Over"
+wrapper.appendChild(butn_Reset)
+
+butn_Reset.addEventListener('click', resetCanvas)
+
+function resetCanvas(){
+ window.location.reload(true);
+}
+
+//----------------------real paintbrush effect----------------------------------
+// e mousedown, mouseenter, and mouseup even
+// onmousedown="colorMe()"
+function removeTheEventListener(){
+  cont.removeEventListener("mouseover", colorMe);
+}
+cont.addEventListener('mousedown', colorMe)
+palette.addEventListener("click", chooseColor)
+cont.addEventListener("click", colorMe)
 
 
+//-------start session with a fresh selection of paint chips-------------------
 
-
-
-
-
-
-
+button_click()
 })
